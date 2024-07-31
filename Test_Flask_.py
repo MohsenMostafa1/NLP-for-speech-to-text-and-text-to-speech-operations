a7c1f381fdadf0e7e297086c9dbe3078de1a4dda
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2024-07-31 21:27:38 +0300

    Update Test_Flask_.py

--- Test_Flask_.py
+++ Test_Flask_.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://192.168.2.30:5000/text-input"
+url = "http://127.0.0.1:5000/text-input"
 
 data = {"input": "How many items do we have in San Diego?"}
 
